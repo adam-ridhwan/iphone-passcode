3cb78012de9af6b2374e6031e839c109f721178b
Adam Ridhwan <adamridhwan.1@gmail.com>
2023-06-26 17:54:02 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className='iphone'>
+          <div className='screen'></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
